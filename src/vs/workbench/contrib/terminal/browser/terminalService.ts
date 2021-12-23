a654d1c028c5ae59152fd44e2ca50b6eabb99396
meganrogge <merogge@microsoft.com>
2021-12-22 21:57:39 -0600

    test

--- src/vs/workbench/contrib/terminal/browser/terminalService.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalService.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+fsdfsdf
 import * as dom from 'vs/base/browser/dom';
 import { timeout } from 'vs/base/common/async';
 import { debounce } from 'vs/base/common/decorators';
